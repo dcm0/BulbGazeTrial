de4e4747642b7b20d13f13aeb2dc81df707621de
dcm0 <donald.mcmillan@dsv.su.se>
2022-01-20 17:10:26 +0100

    Backend functionally complete
    Need the dashboard HTML and a lot of debugging

--- server/bulbServer.js
+++ server/bulbServer.js
@@ -1,7 +1,9 @@
 
 
+const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
 const app = require('express');
 const { runInThisContext } = require('vm');
+const logger = require('pino')(pino.destination('./bulbLogs.log'))
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
   cors: {
@@ -9,14 +11,20 @@ const io = require('socket.io')(http, {
   }
 });
 
+//Status Variables
+last_differences = 2;
+current_target;
+round_counter = 0;
+
 
 
 //Bulb Controller object 
-
 class bulbController {
 
-  constructor(namespace) {
+  constructor(namespace, controlnsp, dashnsp) {
     this.nsp = namespace;
+    this.controlnsp = controlnsp;
+    this.dashnsp = dashnsp;
     this.stateMachine = 0;
     this.counter = 0;
     this.max_trigger = 4;
@@ -26,18 +34,24 @@ class bulbController {
     this.processing = false;
     this.last_pitch = 0;
     this.last_yaw = 0;
+    this.lightOn = false;
+    this.log = logger.child({ camera: this.nsp.name });
 
     this.nsp.on("face", this.nextFrame); //no 100% sure on this one...
+    this.nsp.on('bulb', this.statusHandler);
 
   }
 
   setPattern(gaze_pattern) {
-    console.log("Updating pattern on " + this.nsp.name + " to " + gaze_pattern);
+    this.log.info("Updating pattern on " + this.nsp.name + " to " + gaze_pattern);
     this.pattern = gaze_pattern.split(" ");
+    this.stateMachine = 0;
+    this.t_cooldown = Date.now()
   }
 
   setAverageFace(avgFace) {
     this.average_face = avgFace;
+    this.log.info('set average face');
   }
 
   compare_numbers_linear(_compare, average, _sum) {
@@ -48,6 +62,24 @@ class bulbController {
       return (true);
     } else {
       return (false);
+    }
+  }
+
+  setState(newLight) {
+    if (newLight != this.lightOn) {
+      this.nsp.emit('bulb', "{command:'toggle'}");
+    }
+  }
+
+  async statusHandler(json_data) {
+
+    payload = JSON.parse(json_data);
+    switch (payload['command']) {
+      case "status":
+        //Update the object and send to dashboard
+        this.lightOn = parseInt(payload['light']) == 0 ? false : true;
+        this.dashnsp.emit('bulb', "{bulb:'" + this.nsp.name + "', status:'" + this.lightOn.toString() + "'}");
+        break;
     }
   }
 
@@ -64,8 +96,8 @@ class bulbController {
       return;
     }
 
-    //GET THE FACE OUT OF THE RAW DATA (JSON OR WHAT?)
-    face = rawface;
+    //GET THE FACE OUT OF THE RAW DATA
+    face = JSON.parse(rawface)[0];
 
     if ((Date.now() - this.t_cooldown) > this.cooldown) {
       var yaw = face["gaze"]["yaw"];
@@ -90,10 +122,12 @@ class bulbController {
               this.state_machine++;
               this.t_cooldown = Date.now(); //got a good look, reset cooldown
               console.log('Moving to state machine in state number ' + this.state_machine);
+              this.log.info('UP, moving to ' + this.state_machine);
               //progressBarUpdate(this.state_machine, this.pattern.length);
             }
           } else {
             this.state_machine = 0;
+            this.log.info('UP, moving to ' + this.state_machine);
             console.log('state machine resetet to 0 fro face missalignment');
           }
           break;
@@ -101,12 +135,14 @@ class bulbController {
           if ((compare_numbers_linear(face_yaw, this.average_face.face_yaw, percentage + 10)) && (compare_numbers_linear(face_pitch, this.average_face.face_pitch, percentage + 10))) {
             if (pitch < (this.last_pitch - percentage)) {
               this.state_machine++;
+              this.log.info('DOWN, moving to ' + this.state_machine);
               this.t_cooldown = Date.now(); //got a good look, reset cooldown
               console.log('Moving to state machine in state number ' + this.state_machine);
-              progressBarUpdate(this.state_machine, this.pattern.length);
+              //progressBarUpdate(this.state_machine, this.pattern.length);
             }
           } else {
             this.state_machine = 0;
+            this.log.info('DOWN, moving to ' + this.state_machine);
             console.log('state machine resetet to 0 fro face missalignment');
           }
           break;
@@ -114,14 +150,16 @@ class bulbController {
           if ((compare_numbers_linear(face_yaw, this.average_face.face_yaw, percentage + 10)) && (compare_numbers_linear(face_pitch, this.average_face.face_pitch, percentage + 10))) {
             if ((compare_numbers_linear(face_yaw, this.average_face.yaw, percentage)) && (compare_numbers_linear(face_pitch, this.average_face.pitch, percentage))) {
               this.state_machine++;
+              this.log.info('CENTER, moving to ' + this.state_machine);
               this.t_cooldown = Date.now(); //got a good look, reset cooldown
               console.log('Moving to state machine in state number ' + this.state_machine);
-              progressBarUpdate(this.state_machine, this.pattern.length);
+              //progressBarUpdate(this.state_machine, this.pattern.length);
               this.last_pitch = this.average_face.pitch;
               this.last_yaw = this.average_face.yaw;
             }
           } else {
             this.state_machine = 0;
+            this.log.info('CENTER, moving to ' + this.state_machine);
             console.log('state machine resetet to 0 fro face missalignment');
           }
           break;
@@ -129,12 +167,14 @@ class bulbController {
           if ((compare_numbers_linear(face_yaw, this.average_face.face_yaw, percentage + 10)) && (compare_numbers_linear(face_pitch, this.average_face.face_pitch, percentage + 10))) {
             if (yaw > (this.last_yaw + percentage)) {
               this.state_machine++;
+              this.log.info('LEFT, moving to ' + this.state_machine);
               this.t_cooldown = Date.now(); //got a good look, reset cooldown
               console.log('Moving to state machine in state number ' + this.state_machine);
-              progressBarUpdate(this.state_machine, this.pattern.length);
+              //progressBarUpdate(this.state_machine, this.pattern.length);
             }
           } else {
             this.state_machine = 0;
+            this.log.info('LEFT, moving to ' + this.state_machine);
             console.log('state machine resetet to 0 fro face missalignment');
           }
           break;
@@ -142,12 +182,14 @@ class bulbController {
           if ((compare_numbers_linear(face_yaw, this.average_face.face_yaw, percentage + 10)) && (compare_numbers_linear(face_pitch, this.average_face.face_pitch, percentage + 10))) {
             if (yaw < (this.last_yaw - percentage)) {
               this.state_machine++;
+              this.log.info('RIGHT, moving to ' + this.state_machine);
               this.t_cooldown = Date.now(); //got a good look, reset cooldown
               console.log('Moving to state machine in state number ' + this.state_machine);
-              progressBarUpdate(this.state_machine, this.pattern.length);
+              //progressBarUpdate(this.state_machine, this.pattern.length);
             }
           } else {
             this.state_machine = 0;
+            this.log.info('RIGHT, moving to ' + this.state_machine);
             console.log('state machine resetet to 0 fro face missalignment');
           }
           break;
@@ -156,21 +198,24 @@ class bulbController {
       }
 
       this.nsp.emit('progress', "{x:'" + this.state_machine + "', outof:'" + this.pattern.length + "'}");
+      this.log.info('PROGRESS, x:' + this.state_machine + " outof: " + this.pattern.length);
       if (this.state_machine == 1) this.nsp.emit('interaction starts'); //not sure this one is needed?
       if (this.state_machine == this.pattern.length) {
         this.state_machine = 0;
         console.log('interaction complete');
+        this.log.info('INTERACTION SUCCESS');
         this.t_cooldown = Date.now();
         this.nsp.emit('interaction complete'); //bulb should do something when it gets this.
       }
 
 
       this.processing = false;
-    }else{
+    } else {
       console.log('timeout');
-      this.state_machine=0;
+      this.log.info('TIMEOUT');
+      this.state_machine = 0;
       this.nsp.emit('progress', "{x:'" + this.state_machine + "', outof:'" + this.pattern.length + "'}");
-   } 
+    }
 
   }
 }
@@ -179,51 +224,147 @@ class bulbController {
 
 var bulbControllers = [];
 
-
+//Connect everything up
 var cnsp = io.of('/controller');
 cnsp.on('connection', function (socket) {
   console.log('controller connected');
+  logger.info('controller connected');
   cnsp.emit('bulb', 'Hello controller');
 });
 
 var dnsp = io.of('/dashboard');
 dnsp.on('connection', function (socket) {
   console.log('dash connected');
+  logger.info('Dashboard connected');
   dnsp.emit('bulb', 'Hello dash!');
+
 });
 
 var cameras = io.of(/^\/camera-\d+$/);
 cameras.on("connection", (socket) => {
   console.log('bulb connected');
-  bulbControllers.push(new bulbController(socket.nsp));
+  bulbControllers.push(new bulbController(socket.nsp, cnsp, dnsp));
+  //What is the Average Face stuff? Where do I get that?
+  logger.info('Bulb connected ' + socket.nsp.name);
   cameras.emit('bulb', 'Hello camera ' + socket.nsp.name);
+});
 
+//Dashboard Event handlers 
+dnsp.on('game', (json_data) => {
+
+  payload = JSON.parse(json_data);
+  switch (payload['command']) {
+    case "skip":
+      logger.info('DASHBOARD SKIP');
+      //record this as a cancel/skip and notify to start the waiting screen on controller
+      cnsp.emit('game', "{command:'passCheck'");
+      //then just do the new pattern command below
+    case "newQuiz":
+      logger.info('DASHBOARD New Quiz');
+      setupNewQuiz(payload['differences']);
+      break;
+    case "setInteraction":
+      for (let index = 0; index < bulbControllers.length; index++) {
+        bulbControllers[index].setPattern(payload['interaction_pattern']); 
+      }
+      logger.info('Interaction Changed ' + payload['interaction_pattern']);
+      break;
+    case "resetCounter":
+      round_counter=0;
+      break;
+    case "logString":
+      //Figure we might want to send participant ID to the logs or something
+      logger.info(payload['logString']);
+  }
 
 });
 
-// io.on('connection', (socket) => {
+//Controller Event handlers 
+cnsp.on('game', (json_data) => {
 
-//   console.log('Connected');
-//   console.log(socket.id);
-//   console.log("JWT token test: ",socket.handshake.headers)
+  payload = JSON.parse(json_data);
+  switch (payload['command']) {
+    case "check":
+      //check if the pattern matches
+      if(checkQuiz()){
+        logger.info('CONTROLLER CHECK - Pass');
+        cnsp.emit('game', "{command:'passCheck'");
+        setupNewQuiz();
+      }else{
+        logger.info('CONTROLLER CHECK - Fail');
+        cnsp.emit('game', "{command:'failCheck'");
+      }
+      break;
+    case "skip":
+      logger.info('CONTROLLER SKIP');
+      cnsp.emit('game', "{command:'skipInitiated'");
+      //record this as a cancel/skip   
+      setupNewQuiz();
+  }
 
-//   socket.on('event_name', (data) => {
+});
 
-//     console.log("Message from Client : ", data);
 
-//     socket.broadcast.emit("Send Message socket.broadcast.emit : ", data);
-//     io.emit("Send Message io.emit Broadcasted : ", data);
-//     socket.emit("Send Message : ", data);
+function checkQuiz(){
 
-//   })
+  correct = true;
+  for (let index = 0; index < bulbControllers.length; index++) {
+    if(bulbControllers[index].lightOn != target_pattern[index]){
+      correct = false;
+    }
+  }
+  return false;
 
-//   socket.on('disconnect', () => {
+}
 
-//     console.log('Disconnected');
 
-//   })
+function setupNewQuiz(differences = last_differences) {
 
-// })
+  //randomise status of lights. 
+  //store as target
+  //change the correct number of lights
+  //set as current status
+
+  last_differences = differences;
+
+  var target_pattern = new Array(bulbControllers.length).fill(false);
+  for (let idx = 0; idx < target_pattern.length; idx++) {
+    target_pattern[idx] = Math.random() < 0.5;
+  }
+
+  var start_pattern = target_pattern;
+
+  if (differences > bulbControllers.length) {
+    differences = bulbControllers.length;
+  }
+
+  while (change_indexes.length < differences) {
+    var random_number = Math.floor(Math.random() * (bulbControllers.length));
+    if (change_indexes.indexOf(random_number) == -1) {
+      // Yay! new random number
+      change_indexes.push(random_number);
+    }
+
+    for (let i = 0; i < change_indexes.length; i++) {
+      //toggle the change ones
+      start_pattern[change_indexes[i]] = !start_pattern[change_indexes[i]]
+    }
+
+    current_target = target_pattern;
+
+    for (let index = 0; index < bulbControllers.length; index++) {
+      bulbControllers[index].setState(start_pattern[index]);
+    }
+    round_counter++;
+    var target_json = "{command:'newQuiz', round:'"+round_counter+"', target_pattern:"+JSON.stringify(target_pattern)+"}";
+    dnsp.emit('game', target_json);
+    cnsp.emit('game', target_json);
+    logger.info('Starting new round ('+round_counter+") differences:"+differences+" elements to change:"JSON.stringify(change_indexes));
+
+  }
+}
+
+
 
 http.listen(8080, () => {
 
