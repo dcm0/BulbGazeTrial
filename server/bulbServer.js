81d0caeecd8a8aa53f54dfa499fc79cf792b33f6
dcm0 <donald.mcmillan@dsv.su.se>
2022-10-20 10:33:13 +0200

    file testing

--- server/bulbServer.js
+++ server/bulbServer.js
@@ -27,6 +27,9 @@ var bulbControllers = [];
 
 
 function checkFileExists(file) {
+  console.log(fs.promises.access(file, fs.constants.F_OK)
+  .then(() => true)
+  .catch(() => false));
   return fs.promises.access(file, fs.constants.F_OK)
            .then(() => true)
            .catch(() => false)
